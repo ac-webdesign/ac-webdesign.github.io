e372d6ae6b2963874bf218cb166d00c1d787fa93
ac-webdesign <ac.masterdesign@gmail.com>
2024-05-15 02:19:38 +0300

    javascript projects added

---
+++ javascript-projects.js
@@ -0,0 +1,33 @@
+
+//POPUP WINDOW WHEN OPENING THE PROJECT
+
+function openPopup(popupId) {
+    var popup = document.getElementById(popupId);
+    var width = 500;
+    var height = 400;
+    var left = (screen.width - width) / 2;
+    var top = (screen.height - height) / 2;
+    var options = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
+    window.open('', '_blank', options).document.write(popup.innerHTML);
+}
+
+// Attach event listeners to thumbnails
+document.getElementById('thumbnail1').addEventListener('click', function() {
+    openPopup('popup1');
+});
+document.getElementById('thumbnail2').addEventListener('click', function() {
+    openPopup('popup2');
+});
+
+function openMemoryWindow() {
+    window.open('memory-game.html', '_blank');
+}
+
+function openSlotsWindow(){
+    window.open('slots-game.html', '_blank');
+
+}
+function openRouletteWindow(){
+    window.open('roulette.html', '_blank');
+
+}
