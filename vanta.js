b881064e35fc8789ac066bbcaaed4f3e6e17c4f2
ac-webdesign <ac.masterdesign@gmail.com>
2024-07-05 23:19:04 +0300

    projects added

---
+++ vanta.js
@@ -0,0 +1,59 @@
+VANTA.NET({
+    el: "#contact",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xfd0000,
+    backgroundColor: 0x101011,
+    points: 5.00,
+    spacing: 14.00
+})
+
+VANTA.NET({
+    el: "#home",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xfd5600,
+    backgroundColor: 0x101011,
+    points: 4.00,
+    spacing: 12.00
+})
+
+
+VANTA.NET({
+    el: "#projects",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xfd5600,
+    backgroundColor: 0x101011,
+    points: 5.00,
+    spacing: 14.00
+})
+VANTA.NET({
+    el: "#about",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xfd5600,
+    backgroundColor: 0x101011,
+    points: 5.00,
+    spacing: 14.00
+})
